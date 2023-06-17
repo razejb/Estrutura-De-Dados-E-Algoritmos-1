b6aebb7b86a261aa4d17dea33e48ac80b1c94f23
razejb <93951585+razejb@users.noreply.github.com>
2023-06-17 18:16:41 +0100

    Update 2.EDA_MaxElementLinkedList.c

--- LinkedList/2.EDA_MaxElementLinkedList.c
+++ LinkedList/2.EDA_MaxElementLinkedList.c
@@ -4,77 +4,77 @@
 |                                                              |
 --------------------------------------------------------------*/
 
-
-
 #include <stdio.h>
 #include <stdlib.h>
 
-
+// Define a estrutura para um nó da lista ligada
 struct Node
 {
     int data;
-    struct Node *next;  
-    
-}
-*first = NULL;
+    struct Node *next;
+};
 
+// Variável global para o primeiro nó da lista ligada
+struct Node *first = NULL;
+
+// Função para criar uma lista ligada a partir de um array de inteiros
 void create(int A[], int n)
 {
     int i;
     struct Node *t, *last;
 
-    first = (struct Node *)malloc(sizeof(struct Node)) ;
+    // Cria o primeiro nó
+    first = (struct Node *)malloc(sizeof(struct Node));
     first->data = A[0];
     first->next = NULL;
     last = first;
 
+    // Cria e conecta os nós restantes
     for (i = 1; i < n; i++)
     {
-        t = (struct Node*)malloc(sizeof(struct Node));
-        t->data = A[i] ;
-        t->next=NULL ;
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
         last->next = t;
         last = t;
     }
 }
 
+// Função para encontrar o valor máximo na lista ligada (iterativa)
 int Max(struct Node *p)
 {
     int max = 0;
 
-    while(p)
+    while (p)
     {
-        if(p->data > max)
+        if (p->data > max)
             max = p->data;
-            p = p->next;
+        p = p->next;
     }
     return max;
-
 }
 
-// Recursiva
+// Função para encontrar o valor máximo na lista ligada (recursiva)
 int RMax(struct Node *p)
 {
     int x = 0;
-    if(p==0)
+    if (p == 0)
         return 0;
-    x = RMax(p->next) ;
+    x = RMax(p->next);
 
-    if(x > p->data)
+    if (x > p->data)
         return x;
-    else    
+    else
         return p->data;
 }
 
-
-
-
-int main() 
+int main()
 {
-    int A[]={3, 5, 7, 10, 25, 8, 32, 2};
+    int A[] = {3, 5, 7, 10, 25, 8, 32, 2};
     create(A, 8);
     
-    printf("Max %d\n", Max(first)) ;
+    printf("Max %d\n", Max(first));
 
     return 0;
 }
+
