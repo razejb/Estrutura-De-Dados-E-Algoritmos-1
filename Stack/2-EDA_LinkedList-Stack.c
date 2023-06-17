2f4fd69b5fd9c48f8b60efd5a7ae01c25ddeb57e
razejb <93951585+razejb@users.noreply.github.com>
2023-06-17 22:59:59 +0100

    Update 2-EDA_LinkedList-Stack.c

--- Stack/2-EDA_LinkedList-Stack.c
+++ Stack/2-EDA_LinkedList-Stack.c
@@ -5,25 +5,25 @@
 --------------------------------------------------------------*/
 
 
-
 #include <stdio.h>
 #include <stdlib.h>
 
+// Definição da estrutura Node (nó)
 struct Node
 {
-    int data;
-    struct Node *next;
-}
-*top = NULL;
+    int data;           // valor armazenado no nó
+    struct Node *next;  // ponteiro para o próximo nó
+} *top = NULL;
 
+// Função para empilhar (inserir) um elemento no topo da pilha
 void push(int x)
 {
     struct Node *t;
 
     t = (struct Node*)malloc(sizeof(struct Node));
 
-    if(t == NULL)
-        printf("stack is full\n") ;
+    if (t == NULL)
+        printf("A pilha está cheia\n");
     else
     {
         t->data = x;
@@ -32,13 +32,14 @@ void push(int x)
     }
 }
 
+// Função para desempilhar (remover) um elemento do topo da pilha e retornar o seu valor
 int pop()
 {
     struct Node *t;
     int x = -1;
 
-    if(top == NULL)
-        printf("stack is Empty\n") ;
+    if (top == NULL)
+        printf("A pilha está vazia\n");
     else
     {
         t = top;
@@ -47,33 +48,32 @@ int pop()
         free(t);
     }
     return x;
-
 }
 
+// Função para exibir os elementos da pilha
 void Display()
 {
     struct Node *p;
     p = top;
-    while(p != NULL)
+    while (p != NULL)
     {
         printf("%d ", p->data);
         p = p->next;
     }
-    printf("\n") ;
+    printf("\n");
 }
 
 int main()
 {
-
     push(10);
     push(13);
     push(17);
     push(22);
 
-    printf("%d \n", pop()) ;
+    printf("%d\n", pop());
 
-    Display() ;
+    Display();
 
     return 0;
-
 }
+
