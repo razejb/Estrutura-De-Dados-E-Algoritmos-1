7168723b0f43f632b69a860e2a08836b934ddd85
razejb <93951585+razejb@users.noreply.github.com>
2023-06-18 01:34:18 +0100

    Update 3-EDA_LinkedListQueue.c

--- Queue/3-EDA_LinkedListQueue.c
+++ Queue/3-EDA_LinkedListQueue.c
@@ -8,27 +8,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Definição da estrutura Node (nó)
 struct Node
 {
-    int data;
-    struct Node *next;
+    int data;           // valor do nó
+    struct Node *next;  // ponteiro para o próximo nó
 }
-*front = NULL, *rear = NULL ;
+*front = NULL, *rear = NULL;
 
+// Função para inserir um elemento no final da fila
 void enqueue(int x)
 {
     struct Node *t;
-    t = (struct Node*)malloc(sizeof(struct Node));
-    if(t == NULL) 
-        printf("Queue is full\n");
-
+    t = (struct Node *)malloc(sizeof(struct Node));
+    if (t == NULL)
+        printf("A fila está cheia\n");
     else
     {
         t->data = x;
         t->next = NULL;
-        if(front == NULL)
+        if (front == NULL)
             front = rear = t;
-        
         else
         {
             rear->next = t;
@@ -37,15 +37,15 @@ void enqueue(int x)
     }
 }
 
+// Função para desenfileirar remover um elemento da frente da fila e retornar o seu valor
 int dequeue()
 {
     int x = -1;
 
     struct Node *t;
 
-    if(front == NULL)
-        printf("Queue is Empty\n");
-
+    if (front == NULL)
+        printf("A fila está vazia\n");
     else
     {
         x = front->data;
@@ -56,32 +56,29 @@ int dequeue()
     return x;
 }
 
-
+// Função para exibir os elementos da fila
 void Display()
 {
     struct Node *p = front;
 
-    while(p)
+    while (p)
     {
-        printf("%d ", p->data) ;
+        printf("%d ", p->data);
         p = p->next;
     }
-    printf("\n") ;
+    printf("\n");
 }
-
 
 int main()
 {
+    enqueue(5);
+    enqueue(10);
+    enqueue(12);
+    enqueue(23);
 
-    enqueue(5) ;
-    enqueue(10) ;
-    enqueue(12) ;
-    enqueue(23) ;
-    
+    Display();
 
-    Display() ;
-
-    printf("%d ", dequeue()) ;
+    printf("%d ", dequeue());
 
     return 0;
 }
