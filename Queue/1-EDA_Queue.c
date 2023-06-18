60c64a659f78a31eaa5b1fef9cc6684f6bf3a403
razejb <93951585+razejb@users.noreply.github.com>
2023-06-18 01:30:45 +0100

    Update 1-EDA_Queue.c

--- Queue/1-EDA_Queue.c
+++ Queue/1-EDA_Queue.c
@@ -8,26 +8,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Definição da estrutura Queue (fila)
 struct Queue
 {
-    int size;
-    int front;
-    int rear;
-    int *Q;
+    int size;   // tamanho da fila
+    int front;  // posição do elemento dianteiro
+    int rear;   // posição do elemento traseiro
+    int *Q;     // array para armazenar os elementos da fila
 };
 
+// Função para criar a fila
 void create(struct Queue *q, int size)
 {
     q->size = size;
     q->front = q->rear = -1;
-    q->Q=(int *)malloc(q->size*sizeof(int));
+    q->Q = (int *)malloc(q->size * sizeof(int));
 }
 
+// Função para inserir um elemento no final da fila
 void enqueue(struct Queue *q, int x)
 {
-    if(q->rear == q->size-1)
-        printf("Queue is full") ;
-    
+    if (q->rear == q->size - 1)
+        printf("A fila está cheia\n");
     else
     {
         q->rear++;
@@ -35,30 +37,30 @@ void enqueue(struct Queue *q, int x)
     }
 }
 
+// Função para remover um elemento da frente da fila e retornar o seu valor
 int dequeue(struct Queue *q)
 {
-    int x= -1;
+    int x = -1;
     if (q->front == q->rear)
-        printf("Queue is EMpty\n") ;
-
+        printf("A fila está vazia\n");
     else
     {
         q->front++;
-        x = q->Q[q->front] ;
+        x = q->Q[q->front];
     }
     return x;
 }
 
+// Função para exibir os elementos da fila
 void Display(struct Queue q)
 {
     int i;
 
-    for(i = q.front+1; i<=q.rear; i++)
-        printf("%d ", q.Q[i]) ;
+    for (i = q.front + 1; i <= q.rear; i++)
+        printf("%d ", q.Q[i]);
 
-    printf("\n") ;
+    printf("\n");
 }
-
 
 int main()
 {
@@ -66,14 +68,14 @@ int main()
 
     create(&q, 5);
 
-    enqueue(&q, 5) ;
-    enqueue(&q, 10) ;
-    enqueue(&q, 12) ;
-    enqueue(&q, 23) ;
+    enqueue(&q, 5);
+    enqueue(&q, 10);
+    enqueue(&q, 12);
+    enqueue(&q, 23);
 
-    Display(q) ;
+    Display(q);
 
-    printf("%d ", dequeue(&q)) ;
+    printf("%d ", dequeue(&q));
 
     return 0;
 }
